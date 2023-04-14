66dbd675838f77f55a77582826b709f096d8e2cf
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2023-04-14 09:54:16 -0300

    failure nodemail

--- src/routes/payRouter.js
+++ src/routes/payRouter.js
@@ -122,10 +122,10 @@ payRouter.get("/feedback/success", async function (req, res) {
               </head>
               <body style="background-color: #232326; display: flex; margin-top: 80px; flex-direction: column; align-items: center;">
                 <div style="display: flex; flex-direction: column; align-items: center; text-align: center; border: 1px solid black; border-radius: 20px; background-color: #ffffff; padding: 20px;"">
-                  <a style="margin-bottom: 10px;" href="https://main--vocal-douhua-90fce6.netlify.app"><svg className='succes_svg' width="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#232326" d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg></a>
+                  <a style="margin-bottom: 10px;" href="https://storecomputer.netlify.app"><svg className='succes_svg' width="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#232326" d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg></a>
                     <h1 style="margin-bottom: 10px;" >Payment Successful</h1>
                     <img style="max-width: 100%; margin-bottom: 10px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_xXsXXnglKn4YmVFVx39Pd-0LgWqhiUVk5g&usqp=CAU" alt="" className='succes_img'>
-                    <a style="margin-bottom: 10px;" href="https://main--vocal-douhua-90fce6.netlify.app/Products" className="succes_a">Keep Buying</a>
+                    <a style="margin-bottom: 10px;" href="https://storecomputer.netlify.app/Products" className="succes_a">Keep Buying</a>
                     <p style="margin-bottom: 10px;" className="succes_p">COMPUTER STORE</p>
                     <ul style="margin-bottom: 10px; list-style-type: none;" className="succes_ul">          
                       <li className="succes_li">Payment ID: ${paymentId}</li>
@@ -189,10 +189,10 @@ payRouter.get("/feedback/pending", async function (req, res) {
       </head>
       <body style="background-color: #232326; display: flex; margin-top: 80px; flex-direction: column; align-items: center;">
         <div style="display: flex; flex-direction: column; align-items: center; text-align: center; border: 1px solid black; border-radius: 20px; background-color: #ffffff; padding: 20px;"">
-          <a style="margin-bottom: 10px;" href="https://main--vocal-douhua-90fce6.netlify.app/"><svg className='pending_svg' width="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#232326" d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg></a>
+          <a style="margin-bottom: 10px;" href="https://storecomputer.netlify.app/"><svg className='pending_svg' width="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#232326" d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg></a>
           <h1 style="margin-bottom: 10px;" className="pending_h1"> Pending Pay !</h1>
           <img style="max-width: 100%; margin-bottom: 10px;" className='pending_img'src="https://img.freepik.com/fotos-premium/simbolo-signo-exclamacion-azul-atencion-o-icono-signo-precaucion-fondo-problema-peligro-alerta-representacion-3d-senal-advertencia_256259-2831.jpg" alt="">
-          <a style="margin-bottom: 10px;" className="pending_a" href="https://main--vocal-douhua-90fce6.netlify.app/Products">Keep Buying</a>
+          <a style="margin-bottom: 10px;" className="pending_a" href="https://storecomputer.netlify.app/Products">Keep Buying</a>
           <p style="margin-bottom: 10px;" className="pending_p">COMPUTER STORE</p>
         </div>
       </body>
@@ -248,10 +248,10 @@ payRouter.get("/feedback/failure", async function (req, res) {
       </head>
       <body style="background-color: #232326; display: flex; margin-top: 80px; flex-direction: column; align-items: center;">
         <div style="display: flex; flex-direction: column; align-items: center; text-align: center; border: 1px solid black; border-radius: 20px; background-color: #ffffff; padding: 20px;"">
-        <a style="margin-bottom: 10px;" href="https://main--vocal-douhua-90fce6.netlify.app"><svg className='failure_svg' width="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#232326" d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg></a>
+        <a style="margin-bottom: 10px;" href="https://storecomputer.netlify.app"><svg className='failure_svg' width="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path style="fill:#232326" d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg></a>
         <h1 style="margin-bottom: 10px;" className="failure_h1"> Failure Pay!</h1>
         <img style="max-width: 100%; margin-bottom: 10px;" className="failure_img" src="https://static.vecteezy.com/system/resources/thumbnails/017/178/563/small/cross-check-icon-symbol-on-transparent-background-free-png.png" alt="">
-        <a style="margin-bottom: 10px;" href="https://main--vocal-douhua-90fce6.netlify.app/Products" className="failure_a">Keep Buying</a>
+        <a style="margin-bottom: 10px;" href="https://storecomputer.netlify.app/Products" className="failure_a">Keep Buying</a>
         <p style="margin-bottom: 10px;" className="failure_p">COMPUTER STORE</p>
       </div>
       </body>
