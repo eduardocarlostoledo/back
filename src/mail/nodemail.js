f8ec6cf729616c97da96a3bb24629ab9ae89b6ab
eduardocarlostoledo <eduardocarlostoledo@gmail.com>
2023-04-14 09:49:22 -0300

    failure nodemail

--- src/mail/nodemail.js
+++ src/mail/nodemail.js
@@ -1,35 +1,35 @@
-const nodemailer = require('nodemailer')
+// const nodemailer = require('nodemailer')
 
-enviarMail = async (producto, precio, usuario, statusId) => {
+// enviarMail = async (producto, precio, usuario, statusId) => {
 
-    let cantidades = producto.split(',')
+//     let cantidades = producto.split(',')
 
-    const config = {
-        host: 'smtp.gmail.com',
-        port: 587,
-        auth: {
-            user: 'computer.store.original@gmail.com',
-            pass: 'ahqdrlukahufvbnt'
-        }
-    }
+//     const config = {
+//         host: 'smtp.gmail.com',
+//         port: 587,
+//         auth: {
+//             user: 'computer.store.original@gmail.com',
+//             pass: 'ahqdrlukahufvbnt'
+//         }
+//     }
 
-    const mensaje = {
-        from: 'computer.store.original@gmail.com',
-        to: usuario,
-        subject: 'compra realizada de forma exitosa!' ,
-        text: `muchas gracias por comprar en Computer Store a continuacion adjunto el detalle de su compra:
-        producto/s: ${producto}
-        cantidad de productos: ${cantidades.length}
-        compra total: ${precio}
-        estado compra: ${statusId}
-                `
-    }
+//     const mensaje = {
+//         from: 'computer.store.original@gmail.com',
+//         to: usuario,
+//         subject: 'compra realizada de forma exitosa!' ,
+//         text: `muchas gracias por comprar en Computer Store a continuacion adjunto el detalle de su compra:
+//         producto/s: ${producto}
+//         cantidad de productos: ${cantidades.length}
+//         compra total: ${precio}
+//         estado compra: ${statusId}
+//                 `
+//     }
 
-    const transport = nodemailer.createTransport(config)
+//     const transport = nodemailer.createTransport(config)
 
-    const info = await transport.sendMail(mensaje)
+//     const info = await transport.sendMail(mensaje)
 
-    console.log(info)
-}
+//     console.log(info)
+// }
 
-module.exports = enviarMail
+// module.exports = enviarMail
