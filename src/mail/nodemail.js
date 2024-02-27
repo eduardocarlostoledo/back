cf3dc1dd2e6f39d951931052a850bcde537d77ad
eduardo <eduardocarlostoledo@gmail.com>
2024-02-27 11:28:45 -0300

    fixes en fs extra y en nodemailer

--- src/mail/nodemail.js
+++ src/mail/nodemail.js
@@ -1,3 +1,42 @@
+const nodemailer = require('nodemailer');
+
+const enviarMail = async (producto, precio, usuario, statusId) => {
+    try {
+        let cantidades = producto.split(',');
+
+        const config = {
+            host: 'smtp.gmail.com',
+            port: 587,
+            auth: {
+                user: 'computer.store.original@gmail.com',
+                pass: 'ahqdrlukahufvbnt'
+            }
+        };
+
+        const mensaje = {
+            from: 'computer.store.original@gmail.com',
+            to: usuario,
+            subject: 'compra realizada de forma exitosa!',
+            text: `muchas gracias por comprar en Computer Store a continuacion adjunto el detalle de su compra:
+            producto/s: ${producto}
+            cantidad de productos: ${cantidades.length}
+            compra total: ${precio}
+            estado compra: ${statusId}
+                    `
+        };
+
+        const transport = nodemailer.createTransport(config);
+
+        const info = await transport.sendMail(mensaje);
+
+        console.log(info);
+    } catch (error) {
+        console.error('Error al enviar el correo:', error);
+    }
+};
+
+module.exports = enviarMail;
+
 // const nodemailer = require('nodemailer')
 
 // enviarMail = async (producto, precio, usuario, statusId) => {
